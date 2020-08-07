a85fda27a1be2ce7dd6618fcbb0e975a5a376429
olkhovsky <olkhovskii_av@ascon.ru>
2020-08-07 12:48:29 +0300

    Вернул назад Settings

--- PilotRevitShareListener/Settings.cs
+++ PilotRevitShareListener/Settings.cs
@@ -5,118 +5,12 @@ namespace PilotRevitShareListener
     [Serializable]
     public class Settings
     {
-        private static object _locker = new object();
-        private string _serverUrl;
-        private string _dbName;
-        private string _login;
-        private string _password;
-        private int _licenseCode;
-        private string _sharePath;
-        private double _timeout;
-
-        public string ServerUrl
-        {
-            get
-            {
-                lock (_locker)
-                    return _serverUrl;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _serverUrl = value;
-            }
-        }
-
-        public string DbName
-        {
-            get
-            {
-                lock (_locker)
-                    return _dbName;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _dbName = value;
-            }
-        }
-
-        public string Login
-        {
-            get
-            {
-                lock (_locker)
-                    return _login;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _login = value;
-            }
-        }
-
-        public string Password
-        {
-            get
-            {
-                lock (_locker)
-                    return _password;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _password = value;
-            }
-        }
-
-        public int LicenseCode
-        {
-            get
-            {
-                lock (_locker)
-                    return _licenseCode;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _licenseCode = value;
-            }
-        }
-
-        public string SharePath
-        {
-            get
-            {
-                lock (_locker)
-                    return _sharePath;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _sharePath = value;
-            }
-        }
-
-        public double Timeout
-        {
-            get
-            {
-                lock (_locker)
-                    return _timeout;
-            }
-
-            set
-            {
-                lock (_locker)
-                    _timeout = value;
-            }
-        }
+        public string ServerUrl { get; set; }
+        public string DbName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int LicenseCode { get; set; }
+        public string SharePath { get; set; }
+        public double Timeout { get; set; }
     }
 }
